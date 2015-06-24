5425644e6bfc5f8e07ed179509a1ea5348977204
Chelsea Stats <ChelseaStats@users.noreply.github.com>
2015-06-24 09:20:52 +0100

    moved to pop3

--- index.php
+++ index.php
@@ -1,37 +1,27 @@
 <?php
-	require_once ( 'email.class.php' );
 	require_once ( '../config.php' );
 	require_once ( 'twitteroauth.php' );
-        
-	$dir = DIR_LINK.'/';
-	if (is_dir($dir)) {
-		$files = scandir($dir,1);
-	
-		array_pop($files); // '..'
-		array_pop($files); // '.'
-	
-		foreach($files as $file):
-			
-			$email = new EmailParser( file_get_contents(DIR_LINK.'/'.$file));
-			
-			var_dump($email);
-	
-			$to = $email->getHeader('to');
-			
-			if($to == TOADD ) :
-				$text = $email->getText();
-				
-				$text = str_replace('Morning All!','',$text);
-				$text = str_replace('This Puzzle has no Perfect Solution!','',$text);
-				$text = str_replace('"Best of luck!"','',$text);
-	
-				$yes = tweet($text);
-				if($yes) :
-					unlink($dir.''.$file);
-				endif;
-			endif;
-		endforeach;
-	} 
+ 	$m_box = imap_open ("{localhost:995/pop3/ssl/novalidate-cert}INBOX", POP_USER, POP_PASS);
+	if( $m_box ) {
+	    	// check messages
+		$check = imap_mailboxmsginfo($m_box);
+		// if there is a message in your inbox - process
+		if( $check->Nmsgs > 0 ) { 
+		     	$message = quoted_printable_decode(imap_body($m_box, 1)); 
+		     	$start=strpos($message,'Morning all!');
+			$end = strpos($message,'Best of luck!');
+			$content = substr($message,$start,$end-$start);
+		     	$content = str_replace('Morning all!','',$content);
+			$content = str_replace('Best of luck!','',$content);
+			$text = $content .'Good luck. #CountDownRocks';
+			echo "Message: {$text}";
+			tweet($text);
+		}
+		// delete mail. clear deleted and close
+		imap_delete($m_box, 1);
+		imap_expunge($m_box);
+	     	imap_close($m_box); 
+	}
 	
 	function tweet($message) {
 		$connection = new TwitterOAuth( CountDownKey1, CountDownKey2, CountDownKey3, CountDownKey4);
